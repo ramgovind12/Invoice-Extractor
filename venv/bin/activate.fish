ad003acca1a3c0bcefbed8ea72354280336d2503
Ram Govind V <ramgovindv@Rams-MacBook-Air.local>
2024-10-07 20:28:41 +0530

    c

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ramgovindv/Desktop/Invoice Extractor/venv"
+set -gx VIRTUAL_ENV "/Users/ramgovindv/Desktop/Invoice-Extractor/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
